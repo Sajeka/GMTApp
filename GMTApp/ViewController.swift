93fff1eca31106ba6ad352fa0bd000f3bc4aef5a
Sarina <sbi@touchlogic.dk>
2019-09-11 16:53:32 +0200

    minor changes, linegraph is working, font can be set

--- GMTApp/ViewController.swift
+++ GMTApp/ViewController.swift
@@ -22,17 +22,17 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let graph1: Graph = Graph(graphPoints: [5.0,6.0,5.0,6.0,1.0,6.0,5.0,6.0,5.0,4.0,7.0], graphColor: nil, lineWidth: 5.0, trendLine: nil, animate: true, animationDuration: 5.0)
-        let graph2: Graph = Graph(graphPoints: [3.0,2.0,3.0,2.0,3.0,2.0,3.0,2.0,3.0,2.0,3.0], graphColor: .cyan, lineWidth: 5.0, trendLine: TrendLine(trendLineColor: .red, trendLineWidth: 3.0, animate: false, animationDuration: nil), animate: true, animationDuration: nil)
+        let graph1: Graph = Graph(graphPoints: [5.0,6.0,5.0,6.0,1.0,6.0,5.0,6.0,5.0,4.0,7.0], graphColor: .blue, lineWidth: 1.0, trendLine: nil, animate: true, animationDuration: 5.0)
+        let graph2: Graph = Graph(graphPoints: [3.0,2.0,3.0,2.0,3.0,2.0,3.0,2.0,3.0,2.0,3.0], graphColor: .purple, lineWidth: 2.0, trendLine: TrendLine(trendLineColor: .orange, trendLineWidth: 2.0, animate: false, animationDuration: nil), animate: true, animationDuration: 8.0)
         
         lineGraphView.setCornerRadius(width: 8.0, height: 8.0)
         lineGraphView.setBorders(topBorder: 50, bottomBorder: 50, leftMargin: 30, rightMargin: 10)
         lineGraphView.setBackgroundGradient(firstColor: UIColor(red: 0, green: 0.8, blue: 0.2, alpha: 1.0), lastColor: .white)
         lineGraphView.setGraphPoints(graph: graph1)
-        //lineGraphView.setGraphPoints(graph: graph2)
-        lineGraphView.hasBeneathGraphGradient(hasBeneathGraphGradient: true)
-        lineGraphView.setAxes(xAxis: ["M","T","S","T","F","W","B"], yAxisMinValue: 0, yAxisMaxValue: 10, yAxisNumberOfElements: 11, fontSize: nil, textColor: nil, marginToXAxis: 5, marginToYAxis: 5)
-        lineGraphView.addGraphTitle(title: "Test Graph", titleFontSize: 24, titleColor: UIColor.purple)
+        lineGraphView.setGraphPoints(graph: graph2)
+
+        lineGraphView.setAxes(xAxis: ["M","T","S","T","F","W","B"], xAxisFont: nil, yAxisMinValue: 0, yAxisMaxValue: 10, yAxisNumberOfElements: 6, fontSize: nil, textColor: nil, marginToXAxis: 5, marginToYAxis: 5, yAxisFont: nil)
+        lineGraphView.addGraphTitle(title: "Test Graph", titleFont: nil, titleColor: UIColor.purple)
         lineGraphView.setTitlePlacement(left: true, center: false, right: false, horizontalOffset: 0.0, verticalOffset: 0.0)
 
         lineGraphView.setNeedsDisplay()
