a23d5a3d145afb31ca090e692c469deddbe69a78
Sarina <sbi@touchlogic.dk>
2019-10-08 15:56:24 +0200

    Shifted back to linegraph to edit the changes from the bar chart

--- GMTApp/ViewController.swift
+++ GMTApp/ViewController.swift
@@ -12,8 +12,8 @@ import GMTPod
 class ViewController: UIViewController {
     
     
-//    @IBOutlet weak var lineGraphView: LineGraphTest!
-    @IBOutlet weak var lineGraphView: BarChart!
+    @IBOutlet weak var lineGraphView: LineGraphTest!
+//    @IBOutlet weak var lineGraphView: BarChart!
     
     
     override func viewDidLoad() {
@@ -26,64 +26,60 @@ class ViewController: UIViewController {
        /*
          Line Graph code
  */
-//         let graph1Animation = LineGraphTest.Animation(animate: LineGraphTest.Animate.animateOnlyTrendLine, animationDuration: 3.0)
-//         let graph1Trendline = TrendLine(trendLineColor: .green, trendLineWidth: 1.0/*, animation: graph1Animation*/)
-//         let graph1: Graph = Graph(graphPoints: [25.0,12.0,75.0,49.0,73.0], graphColor: .blue, graphTitle: "Shoes", lineWidth: 1.0, trendLine: graph1Trendline, circleRadius: 3.0/*, animation: graph1Animation*/)
-//         let graph2Trendline = TrendLine(trendLineColor: .orange, trendLineWidth: 2.0/*, animation: graph1Animation*/)
-//         let graph2: Graph = Graph(graphPoints: [32.0,18.0,26.0,20.0,15.0], graphColor: .purple, graphTitle: "Bags", lineWidth: 2.0, trendLine: graph2Trendline, circleRadius: 3.0/*, animation: graph1Animation*/)
-//
-//
-//         lineGraphView.setCornerRadius(width: 8.0, height: 8.0)
-//         lineGraphView.setBorders(topBorder: 50, bottomBorder: 50, leftBorder: 35, rightBorder: 35)
-//         //        lineGraphView.setBackgroundGradient(firstColor: UIColor(red: 0, green: 0.8, blue: 0.2, alpha: 1.0), lastColor: .white)
-//         lineGraphView.setGraphPoints(graph: graph1)
-//         lineGraphView.setGraphPoints(graph: graph2)
-//
-//         lineGraphView.setAxes(xAxis: ["Man","Tir","Ons","Tor","Fre"], xAxisFont: nil, yAxisMinValue: 0, yAxisMaxValue: 100, yAxisNumberOfElements: 5, fontSize: 11, textColor: nil, marginToXAxis: 10, marginToYAxis: 5, yAxisFont: nil)
-//         lineGraphView.addGraphTitle(title: "Ugentllig salg", titleFont: nil, titleColor: UIColor.black)
-//         lineGraphView.setTitlePlacement(left: true, center: false, right: false, horizontalOffset: 0.0, verticalOffset: 0.0)
-//         //        lineGraphView.setXValuesRotationInDegrees(degree:  Double(285))
-//         lineGraphView.setHelperLinesVisible(shouldHelperLinesBeVisible: true)
-//         let test = LineGraphTest.CustomizedTextBox(cornerRadius: nil,
-//         font: nil,
-//         textSize: nil,
-//         textColor: nil,
-//         textBoxColor: .white,
-//         textBoxBorderColor: .black,
-//         textBoxBorderLineWidth: nil,
-//         textBoxHeightPadding: 6,
-//         textBoxWidthPadding: 6,
-//         triangleHeight: nil,
-//         triangleWidth: nil,
-//         triangleTipPadding: nil)
-//         lineGraphView.customizeTextBoxOnPointClick(customizedTextBoxStruct: test)
-//         lineGraphView.addTextToEndOfTextBox(endText: " kr")
-//         lineGraphView.addAnimation(animation: graph1Animation)
-//         lineGraphView.setNeedsDisplay()
+         let graph1Animation = LineGraphTest.Animation(animate: LineGraphTest.Animate.animateOnlyTrendLine, animationDuration: 3.0)
+         let graph1Trendline = TrendLine(trendLineColor: .green, trendLineWidth: 1.0/*, animation: graph1Animation*/)
+         let graph1: Graph = Graph(graphPoints: [25.0,12.0,75.0,49.0,73.0], graphColor: .blue, graphTitle: "Shoes", lineWidth: 1.0, trendLine: graph1Trendline, circleRadius: 3.0/*, animation: graph1Animation*/)
+         let graph2Trendline = TrendLine(trendLineColor: .orange, trendLineWidth: 2.0/*, animation: graph1Animation*/)
+         let graph2: Graph = Graph(graphPoints: [32.0,18.0,26.0,20.0,15.0], graphColor: .purple, graphTitle: "Bags", lineWidth: 2.0, trendLine: graph2Trendline, circleRadius: 3.0/*, animation: graph1Animation*/)
+
+
+         lineGraphView.setCornerRadius(width: 8.0, height: 8.0)
+         lineGraphView.setBorders(topBorder: 50, bottomBorder: 50, leftBorder: 35, rightBorder: 35)
+         //        lineGraphView.setBackgroundGradient(firstColor: UIColor(red: 0, green: 0.8, blue: 0.2, alpha: 1.0), lastColor: .white)
+         lineGraphView.setGraphPoints(graph: graph1)
+         lineGraphView.setGraphPoints(graph: graph2)
+
+         lineGraphView.setAxes(xAxis: ["Man","Tir","Ons","Tor","Fre"], xAxisFont: nil, yAxisMinValue: 0, yAxisMaxValue: 100, yAxisNumberOfElements: 5, fontSize: 11, textColor: nil, marginToXAxis: 10, marginToYAxis: 5, yAxisFont: nil)
+         lineGraphView.addGraphTitle(title: "Ugentllig salg", titleFont: nil, titleColor: UIColor.black)
+         lineGraphView.setTitlePlacement(left: true, center: false, right: false, horizontalOffset: 0.0, verticalOffset: 0.0)
+         //        lineGraphView.setXValuesRotationInDegrees(degree:  Double(285))
+         lineGraphView.setHelperLinesVisible(shouldHelperLinesBeVisible: true)
+                let formatter = NumberFormatter()
+                formatter.numberStyle = .currency
+                lineGraphView.formatInfoBoxText(numberFormatter: formatter)
+
+         lineGraphView.addAnimation(animation: graph1Animation)
+         lineGraphView.setNeedsDisplay()
  
         /*
          Bar graph code
          */
-        let barChart1: BarChartObj = BarChartObj(barChartPoints: [2500.5,1200.0,7500.0,490.0,730.0], barColor: .red, barChartTitle: "Shoes", lineWidth: 1.0)
-        let barChart2: BarChartObj = BarChartObj(barChartPoints: [2000.8,1800.0,500.0,550.0,3000.0], barColor: .blue, barChartTitle: "Bags", lineWidth: 1.0)
-        let barChart3: BarChartObj = BarChartObj(barChartPoints: [3000.3,900.0,620.0,5500.0,4700.0], barColor: .green, barChartTitle: "Perfumes", lineWidth: 1.0)
-
-        lineGraphView.setCornerRadius(width: 5.0, height: 5.0)
-        lineGraphView.setBorders(topBorder: 50, bottomBorder: 50, leftBorder: 35, rightBorder: 35)
-
-        lineGraphView.setBarChartPoints(barChart: barChart1)
-        lineGraphView.setBarChartPoints(barChart: barChart2)
-        lineGraphView.setBarChartPoints(barChart: barChart3)
-
-        lineGraphView.setAxes(xAxis: ["Man","Tir","Ons","Tor","Fre"], xAxisFont: nil, yAxisMinValue: 0, yAxisMaxValue: 10000, yAxisNumberOfElements: 6, fontSize: 11, textColor: nil, marginToXAxis: 10, marginToYAxis: 5, yAxisFont: nil)
-        lineGraphView.setXValuesRotationInDegrees(degree:  Double(270))
-        lineGraphView.addBarChartTitle(title: "Ugentllig salg", titleFont: nil, titleColor: UIColor.black)
-        lineGraphView.setTitlePlacement(titlePlacement: BarChart.TitlePlacement.left, horizontalOffset: 0.0, verticalOffset: 0.0)
-        lineGraphView.setHelperLines(helperLines: BarChart.HelperLines(helperLinesColor: .gray, helperLinesLineWidth: 0.5, helperLinesDashes: (2.0, 2.0)))
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        lineGraphView.formatInfoBoxText(numberFormatter: formatter)
-        lineGraphView.setNeedsDisplay()
+//        let barChart1: BarChartObj = BarChartObj(barChartPoints: [2500.5,1200.0,7500.0,490.0,730.0], barColor: .red, barChartCategory: "Shoes", lineWidth: 1.0)
+//        let barChart2: BarChartObj = BarChartObj(barChartPoints: [2000.8,1800.0,500.0,550.0,3000.0], barColor: .blue, barChartCategory: "Bags", lineWidth: 1.0)
+//        let barChart3: BarChartObj = BarChartObj(barChartPoints: [3000.3,900.0,620.0,5500.0,4700.0], barColor: .green, barChartCategory: "Perfumes", lineWidth: 1.0)
+////        let barChart4: BarChartObj = BarChartObj(barChartPoints: [3080.3,2900.0,7620.0,5010.0,4100.0], barColor: .purple, barChartCategory: "Dresses", lineWidth: 1.0)
+//        let barChartArray = [barChart1, barChart2, barChart3]
+//        let yAxis = BarChart.YAxis(yAxisMinValue: 0, yAxisMaxValue: 8000, yAxisNumberOfElements: 5, yAxisFormatter: BarChart.YAxisFormatter.thousand)
+//        let customizedCategory = BarChart.Category(categoryOrientation: BarChart.CategoryOrientation.vertical, paddingBetweenCategories: 5, categoryColorBoxSize: BarChart.CategoryBoxSize.medium)
+//        let customizedCategoryText = BarChart.TextAttributes(textColor: .black, textFont: UIFont(name: "Avenir-Light", size: 10), textSize: nil)
+//        lineGraphView.setCornerRadius(width: 5.0, height: 5.0)
+//        lineGraphView.adjustBorders(addToTopBorder: 0, addToRightBorder: 0, addToBottomBorder: 5, addToLeftBorder: 0)
+//
+//        lineGraphView.createBarChart(barCharts: barChartArray, xAxisStrings: ["Man","Tir","Ons","Tor"], yAxis: yAxis)
+//        lineGraphView.customizeCategories(customizedCategory: customizedCategory, customizedTextAttributes: customizedCategoryText)
+//        
+//    
+//
+//        lineGraphView.setXValuesRotationInDegrees(degree:  Double(270))
+//        lineGraphView.addBarChartTitle(title: "Ugentllig salg", titleFont: nil, titleColor: UIColor.black)
+//        
+//        lineGraphView.setTitlePlacement(titlePlacement: BarChart.TitlePlacement.left, horizontalOffset: 0.0, verticalOffset: 0.0)
+//        lineGraphView.setHelperLines(helperLines: BarChart.HelperLines(helperLinesColor: .gray, helperLinesLineWidth: 0.5, helperLinesDashes: (2.0, 2.0)))
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .currency
+//        lineGraphView.formatInfoBoxText(numberFormatter: formatter)
+//        
+//        lineGraphView.setNeedsDisplay()
 
     }
 }
